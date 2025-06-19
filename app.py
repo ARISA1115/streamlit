32b366f166b58508b115596c55ace14226765ea3
ARISA1115 <a.engine0808@gmail.com>
2025-06-19 19:12:31 +0900

    [add]line chart using plotly

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.graph_objects as go
 
 st.title("Streamlit app")
 
@@ -50,3 +51,40 @@ st.dataframe(df)
 
 st.subheader("Display DataFrame as Table")
 st.table(df)
+
+st.header("Create Plotly Graph")
+
+data = {
+    "Month": ["Jan", "Feb", "Mar", "Apr", "May", "jun"],
+    "Sales": [100, 120, 140, 180, 200, 210],
+    "Profit": [20, 25, 30, 40, 50, 55]
+}
+
+df = pd.DataFrame(data)
+
+st.write("Sample Data")
+st.dataframe(df) 
+
+fig = go.Figure()
+fig.add_trace(
+    go.Scatter(x=df["Month"], y=df["Sales"], mode="lines+markers", name="Sales", line=dict(color="blue", width=2)))
+fig.add_trace(go.Scatter(x=df["Month"], y=df["Profit"], mode="lines+markers", name="Profit", line=dict(color="red", width=2)))
+
+fig.update_layout(
+    title="Sales and Profit Over Months",
+    xaxis_title="Month",
+    yaxis_title="Amount",
+    font=dict(family="Meiryo", size=12),
+    legend=dict(orientation="h", 
+    yanchor="bottom", y=1.02, 
+    xanchor="right", x=1),
+    hovermode="x unified"
+)
+
+fig.update_xaxes(tickangle=45)
+fig.update_yaxes(
+    zeroline=True,
+    zerolinewidth=2,
+    zerolinecolor="lightgrey")
+
+st.plotly_chart(fig)
